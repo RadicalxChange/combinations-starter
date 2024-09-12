b97a8d2a9996a6e99942e62f94d17793fba69f8f
Alex Randaccio <acrandaccio@gmail.com>
2024-09-12 16:07:34 -0400

    home page and posts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 export default {
   content: ["./*.hbs", "./**/*.hbs"],
   theme: {
+    screens: {
+      md: { raw: "(min-width: 640px)" },
+      lg: { raw: "(min-width: 768px)" },
+      xl: { raw: "(min-width: 900px)" },
+      "base-landscape": {
+        raw: "(max-width: 767px) and (orientation: landscape)",
+      },
+      "lg-tall": { raw: "(min-width: 768px) and (max-aspect-ratio: 2/1)" },
+      touch: { raw: "(hover: none)" },
+    },
+    fontFamily: {
+      display: ["Messer", "san-serif"],
+      body: ["Suisse Intl", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [
